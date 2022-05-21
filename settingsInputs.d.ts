d34c0fe17c5b121bda8f70660bfdf4060a631552
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-05-21 19:33:28 +0200

    TC-51 input types

--- settingsInputs.d.ts
+++ settingsInputs.d.ts
@@ -1,29 +1,43 @@
+export enum InputType {
+	NUMBER = 'NUMBER',
+	TEXT = 'TEXT',
+	CHECKBOX = 'CHECKBOX',
+	DROPDOWN = 'DROPDOWN',
+}
+
 export interface Input {
 	id: string;
 	required: boolean;
+	type: InputType;
 }
 
 export interface NumberInput extends Input {
+	type: InputType.NUMBER;
+	value: number;
 	max?: number;
 	min?: number;
-	value: number;
 }
 
 export interface TextInput extends Input {
+	type: InputType.TEXT;
 	value: string;
 	regex?: RegExp;
 }
 
 export interface CheckboxInput extends Input {
+	type: InputType.CHECKBOX;
 	value: boolean;
 }
 
-export interface DropdownInput<T> extends Input {
+export interface DropdownInput extends Input {
+	type: InputType.DROPDOWN;
 	value: string;
-	options: DropdownOption<T>[];
+	options: DropdownOption[];
 }
 
-export interface DropdownOption<T> {
+export interface DropdownOption {
 	text: string;
-	value: T;
+	value: string;
 }
+
+export type Inputs = (TextInput | NumberInput | CheckboxInput | DropdownInput)[];
