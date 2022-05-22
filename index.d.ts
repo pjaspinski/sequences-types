5235a779a49d3d20e080eb614c42f1b422d4312c
Paweł Jaśpiński <pawel.jaspinski@gmail.com>
2022-05-22 18:43:58 +0200

    TC-62

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,19 @@ import { Input } from './settingsInputs';
 export interface PluginDefinition {
 	name: string;
 	settingsFields: Input[];
+	setup: SetupFunction;
+	destroy: DestroyFunction;
 }
 
 export interface Plugin extends PluginDefinition {
 	id: number;
+	status: PluginStatus;
+	active: boolean;
 }
+
+export type PluginSettings = { [index: string]: string };
+export type SetupFunction = (options: PluginSettings) => void;
+export type DestroyFunction = () => void;
+export type PluginStatus = 'DISABLED' | 'RUNNING' | 'ERROR' | 'LOADING';
 
 export * from './settingsInputs';
